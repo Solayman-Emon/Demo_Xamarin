640f4bc713e5562b384728bf94998590eb4d3cd2
Solayman-Emon <emonqt19@gmail.com>
2021-03-18 13:17:29 +0600

    updt1

--- Demo_Xamarin/ThirdActivity.cs
+++ Demo_Xamarin/ThirdActivity.cs
@@ -1,5 +1,6 @@
 ﻿using Android.App;
 using Android.Content;
+using Android.Media;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
@@ -14,9 +15,12 @@ namespace Demo_Xamarin
     [Activity(Label = "Activity1")]
     public class ThirdActivity : Activity
     {
-        Button bckButton1, countButton;
+        Button bckButton1, strtRecordButton, stpRecordButton;
         TextView countText;
         int count;
+        string pathSave = " ";
+        MediaRecorder mediaRecorder;
+        MediaPlayer mediaPlayer;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -25,10 +29,14 @@ namespace Demo_Xamarin
             // Create your application here
 
             bckButton1 = FindViewById<Button>(Resource.Id.bckButton1);
-            countButton = FindViewById<Button>(Resource.Id.countButton);
-            countText = FindViewById<TextView>(Resource.Id.countText);
+            strtRecordButton = FindViewById<Button>(Resource.Id.strtRecordButton);
+            stpRecordButton = FindViewById<Button>(Resource.Id.stpRecordButton);
 
-            countButton.Click += (sender, e) => { countText.Text = (++count).ToString(); };
+            //countButton = FindViewById<Button>(Resource.Id.countButton);
+            //countText = FindViewById<TextView>(Resource.Id.countText);
+
+            //countButton.Click += (sender, e) => { countText.Text = (++count).ToString(); };
+
             bckButton1.Click += (sender, e) =>
             {
                 Intent nextActivivty = new Intent(this, typeof(SecondActivity));
